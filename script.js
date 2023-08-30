2f6598b2188463cca19c5bb305ca706ff860d69e
Kiwispin <kiwispin@live.com>
2023-08-30 20:22:43 +1200

    Customise

--- script.js
+++ script.js
@@ -1,43 +1,52 @@
 function rollDice() {
-    // Indicate dice are being rolled
-    const die1Elem = document.getElementById('die1');
-    const die2Elem = document.getElementById('die2');
-    die1Elem.classList.add('shaking');
-    die2Elem.classList.add('shaking');
-    
-    setTimeout(() => {
-        die1Elem.classList.remove('shaking');
-        die2Elem.classList.remove('shaking');
-        
-        const dice1Value = Math.floor(Math.random() * 6) + 1;
-        const dice2Value = Math.floor(Math.random() * 6) + 1;
+    const diceContainer = document.getElementById('diceContainer');
+    diceContainer.innerHTML = ''; // Clear previous dice
 
-        displayDiceValue('die1', dice1Value);
-        displayDiceValue('die2', dice2Value);
+    const diceCount = parseInt(document.getElementById('diceCount').value);
+    const sideCount = parseInt(document.getElementById('sideCount').value);
+    let customValuesInput = document.getElementById('customValues').value.split(',').map(num => parseInt(num.trim()));
+
+    // If customValuesInput is empty or doesn't match the side count, generate default values
+    if (customValuesInput.length !== sideCount || !customValuesInput.every(Number.isInteger)) {
+        customValuesInput = Array.from({ length: sideCount }, (_, i) => i + 1);
+    }
+
+    for (let i = 0; i < diceCount; i++) {
+        const dieElem = document.createElement('div');
+        dieElem.classList.add('die');
+        dieElem.setAttribute('id', `die${i}`);
+        diceContainer.appendChild(dieElem);
+    }
+
+    const diceElems = document.querySelectorAll('.die');
+    diceElems.forEach(dieElem => dieElem.classList.add('shaking'));
+
+    setTimeout(() => {
+        diceElems.forEach(dieElem => {
+            dieElem.classList.remove('shaking');
+
+            const randomValue = Math.floor(Math.random() * sideCount);
+            const diceValue = customValuesInput[randomValue];
+
+            displayDiceValue(dieElem, diceValue);
+        });
     }, 1500); // 0.5s * 3 iterations of shaking animation
 }
 
-function displayDiceValue(dieId, value) {
-    const dieElem = document.getElementById(dieId);
-    dieElem.innerHTML = ''; // Clear existing dots
+function displayDiceValue(dieElem, diceValue) {
+    const positions = [
+        [],
+        ['center'],
+        ['top-left', 'bottom-right'],
+        ['top-left', 'center', 'bottom-right'],
+        ['top-left', 'top-right', 'bottom-left', 'bottom-right'],
+        ['top-left', 'top-right', 'center', 'bottom-left', 'bottom-right'],
+        ['top-left', 'top-right', 'center-left', 'center-right', 'bottom-left', 'bottom-right']
+    ];
 
-    const diceConfig = {
-        1: ['center-center'],
-        2: ['top-left', 'bottom-right'],
-        3: ['top-left', 'center-center', 'bottom-right'],
-        4: ['top-left', 'top-right', 'bottom-left', 'bottom-right'],
-        5: ['top-left', 'top-right', 'center-center', 'bottom-left', 'bottom-right'],
-        6: ['top-left', 'top-right', 'center-left', 'center-right', 'bottom-left', 'bottom-right']
-    };
-
-    diceConfig[value].forEach(position => {
+    positions[diceValue].forEach(pos => {
         const dot = document.createElement('div');
-        dot.classList.add('dot', position);
+        dot.classList.add('dot', pos);
         dieElem.appendChild(dot);
-        
-        // Make dots visible with a short delay to give fade-in effect
-        setTimeout(() => {
-            dot.classList.add('visible');
-        }, 100);
     });
 }
