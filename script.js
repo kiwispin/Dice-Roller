3e80154a2c7aa8ea1c2dd3736a51a73f41ea0c0b
Kiwispin <kiwispin@live.com>
2023-08-31 22:53:37 +1200

    More fixing

--- script.js
+++ script.js
@@ -1,39 +1,124 @@
-function rollDice() {
-    const diceContainer = document.getElementById('diceContainer');
-
-    diceContainer.innerHTML = '';  // Clear previous dice
-    diceContainer.style.opacity = '0';  // Reset the opacity for fade-in effect
-
-    let numDice = parseInt(document.getElementById('numDice').value) || 1;
-    let faces = parseInt(document.getElementById('faces').value) || 6;
-
-    const customValuesInput = document.getElementById('customValues').value;
-    const customValues = customValuesInput ? customValuesInput.split(',').map(Number) : [];
-
-    let diceValues = roll(numDice, faces, customValues);
-    
-    diceValues.forEach(value => {
-        let dieDiv = document.createElement('div');
-        dieDiv.className = 'die';
-        dieDiv.innerText = value;
-        diceContainer.appendChild(dieDiv);
-    });
-
-    // Fade-in effect for dice container
-    setTimeout(() => {
-        diceContainer.style.opacity = '1';
-        diceContainer.style.transform = 'translateY(0px)';
-    }, 100);
+body {
+    font-family: 'Roboto', sans-serif;
+    background-color: #f5f5f5;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+    color: #333;
 }
 
-function roll(numDice, faces, customValues) {
-    let results = [];
-    for (let i = 0; i < numDice; i++) {
-        if (customValues && customValues.length > 0) {
-            results.push(customValues[Math.floor(Math.random() * customValues.length)]);
-        } else {
-            results.push(Math.floor(Math.random() * faces) + 1);
-        }
+.container {
+    background: #ffffff;
+    padding: 30px;
+    border-radius: 8px;
+    box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
+    width: 90%;
+    max-width: 400px;
+}
+
+h1 {
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+/* For labels */
+label {
+    display: block;
+    font-size: 0.9rem;
+    margin-bottom: 5px;
+    color: #444;
+}
+
+.input-group {
+    display: flex;
+    gap: 10px;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+input {
+    padding: 8px 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    margin-bottom: 15px;
+}
+
+#numDice, #faces {
+    width: 80px;
+}
+
+#customValues {
+    flex-grow: 2;
+}
+
+button {
+    background-color: #6200ea;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+    display: block;
+    margin: 0 auto 20px auto; /* Centered with some margin at the bottom */
+    transition: background-color 0.3s ease;
+}
+
+button:hover {
+    background-color: #4b00b5;
+}
+
+.dice-container {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    justify-content: center;
+}
+
+.die {
+    width: 50px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 24px;
+    background-color: #fff;
+    border: 2px solid #ddd;
+    border-radius: 8px;
+    box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
+}
+
+.fade-in {
+    animation: fadeIn ease 1s;
+}
+
+.shake {
+    animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;
+    transform: translate3d(0, 0, 0);
+    backface-visibility: hidden;
+    perspective: 1000px;
+}
+
+@keyframes fadeIn {
+    0% {opacity:0;}
+    100% {opacity:1;}
+}
+
+@keyframes shake {
+    10%, 90% {
+        transform: translate3d(-1px, 0, 0);
     }
-    return results;
+    
+    20%, 80% {
+        transform: translate3d(2px, 0, 0);
+    }
+
+    30%, 50%, 70% {
+        transform: translate3d(-4px, 0, 0);
+    }
+
+    40%, 60% {
+        transform: translate3d(4px, 0, 0);
+    }
 }
