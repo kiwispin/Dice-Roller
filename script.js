a9333c9d157c71c1dd7fc1aa606b625ee4e4b615
Kiwispin <kiwispin@live.com>
2023-08-31 20:13:30 +1200

    Dice shaking

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function rollDice() {
-    console.log("rollDice function called.");
     const diceContainer = document.getElementById('diceContainer');
-    console.log("diceContainer acquired:", diceContainer);
 
     diceContainer.innerHTML = '';  // Clear previous dice
+    diceContainer.style.opacity = '0';  // Reset the opacity for fade-in effect
 
     const numDice = document.getElementById('numDice').value || 1;
     const faces = document.getElementById('faces').value || 6;
     const customValues = (document.getElementById('customValues').value || "").split(',').map(Number);
 
     let diceValues = roll(numDice, faces, customValues);
-    console.log("Dice values:", ...diceValues);
     
     diceValues.forEach(value => {
         let dieDiv = document.createElement('div');
@@ -18,6 +16,12 @@ function rollDice() {
         dieDiv.innerText = value;
         diceContainer.appendChild(dieDiv);
     });
+
+    // Fade-in effect for dice container
+    setTimeout(() => {
+        diceContainer.style.opacity = '1';
+        diceContainer.style.transform = 'translateY(0px)';
+    }, 100);
 }
 
 function roll(numDice, faces, customValues) {
