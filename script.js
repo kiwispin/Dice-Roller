cfd14d86864f8ae6da6958de4801aa4d53f787b2
Kiwispin <kiwispin@live.com>
2023-08-31 13:14:13 +1200

    Fresh

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
 function rollDice() {
-    let diceContainer = document.getElementById("diceContainer");
-
     const numDice = parseInt(document.getElementById("numDice").value) || 1;
     const faces = parseInt(document.getElementById("faces").value) || 6;
     const customValues = document.getElementById("customValues").value.split(",").map(Number);
 
-    let diceValues = [];
+    const diceContainer = document.getElementById("diceContainer");
+    diceContainer.innerHTML = ''; // Clear previous dice
 
     for (let i = 0; i < numDice; i++) {
-        let diceValue;
+        const dieDiv = document.createElement('div');
+        dieDiv.className = 'die';
+
+        let value;
         if (customValues.length > 1) {
-            diceValue = customValues[Math.floor(Math.random() * customValues.length)];
+            value = customValues[Math.floor(Math.random() * customValues.length)];
         } else {
-            diceValue = Math.floor(Math.random() * faces) + 1;
+            value = Math.floor(Math.random() * faces) + 1;
         }
-        diceValues.push(diceValue);
-    }
 
-    displayDiceValue(diceValues);
-}
-
-function displayDiceValue(values) {
-    const diceContainer = document.getElementById("diceContainer");
-    diceContainer.innerHTML = "";  // Clear previous dice
-
-    values.forEach(value => {
-        const dieDiv = document.createElement("div");
-        dieDiv.classList.add("die");
-        dieDiv.innerText = value.toString();
+        dieDiv.innerText = value;
         diceContainer.appendChild(dieDiv);
-    });
+    }
 }
